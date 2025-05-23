48b6f72b7798e9d6c54e5659986e784afcd0fdbe
Joao Victor Franca <joao.victor.franca.new@gmail.com>
2025-05-23 09:11:17 -0700

    initial commit

---
+++ src/main/java/com/project/BasicLoginSystem/BasicLoginSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.BasicLoginSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicLoginSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicLoginSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/BasicLoginSystem/BasicLoginSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.BasicLoginSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicLoginSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
